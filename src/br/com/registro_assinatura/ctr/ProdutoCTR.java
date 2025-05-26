624affb137ca6c27b315ca3ffc3aa6460f6c675c
yoVitt <darkrubber13@gmail.com>
2025-05-26 11:26:57 -0300

    Commit Atualização Code

--- src/br/com/registro_assinatura/ctr/ProdutoCTR.java
+++ src/br/com/registro_assinatura/ctr/ProdutoCTR.java
@@ -20,5 +20,53 @@ public class ProdutoCTR {
     public ProdutoCTR(){
     }
     
+    public String inserirProduto(ProdutoDTO produtoDTO){
+        try{
+            if(produtoDAO.inserirProduto(produtoDTO)){
+                return "Produto Cadastrado com Sucesso!!";
+            }else{
+                return "Produto não cadastrado!";
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return "Produto não Cadastrado";
+        }
+    }
+    
+    public String alterarProduto(ProdutoDTO produtoDTO){
+        try{
+            if (produtoDAO.alterarProduto(produtoDTO)){
+                return "Produto alterado com sucesso";
+            }else{
+                return "Falha ao alterar produto";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Produto Não Alterado!";
+        }
+    }
+    
+    public String excluirProduto(ProdutoDTO produtoDTO){
+        try{
+            if (produtoDAO.excluirProduto(produtoDTO)){
+                return "Produto excluido com sucesso!";
+            }else{
+                return "Produto não Excluido!";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Falha ao excluir produto!";
+        }
+    }
+    
+    public ResultSet consultarProduto(ProdutoDTO produtoDTO, int opcao){
+        ResultSet rs = null;
+        rs = produtoDAO.consultarProduto(produtoDTO, opcao);
+        return rs;
+    }
+    
+    public void CloseDB(){
+        ConexaoDAO.CloseDB();
+    }
     
 }
